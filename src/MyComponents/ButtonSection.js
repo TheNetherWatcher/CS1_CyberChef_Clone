ca1ff751b55359bc634eea3afd9d5d355b204e6e
Siddhesh <cse220001076@iiti.ac.in>
2023-06-11 16:40:06 +0530

    Buttons fulfiling screen

--- src/MyComponents/ButtonSection.js
+++ src/MyComponents/ButtonSection.js
@@ -7,6 +7,17 @@ const ButtonSection = () => {
       <button className="custom-button">Button 2</button>
       <button className="custom-button">Button 3</button>
       <button className="custom-button">Button 4</button>
+      <button className="custom-button">Button 1</button>
+      <button className="custom-button">Button 2</button>
+      <button className="custom-button">Button 3</button>
+      <button className="custom-button">Button 4</button>
+      <button className="custom-button">Button 1</button>
+      <button className="custom-button">Button 2</button>
+      <button className="custom-button">Button 3</button>
+      <button className="custom-button">Button 4</button>
+      <button className="custom-button">Button 1</button>
+      <button className="custom-button">Button 2</button>
+      
     </div>
   );
 };
