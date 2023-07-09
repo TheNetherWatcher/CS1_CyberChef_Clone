046f99178f194971213b649af45f22d975ab9757
bhrkrv2211 <bharatkaurav2211@gmail.com>
2023-07-09 19:13:53 +0530

    Updated Frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React, {useState} from 'react';              
+import React,{useState,useEffect} from 'react'; 
+import { useDencrypt } from 'use-dencrypt-effect';
+import CryptoJS from 'crypto-js';
 import './App.css'
 import './MyComponents/buttonsection.css'
 import './MyComponents/sectioncontent.css'
-import Navbar from './MyComponents/Navbar';           //Importing Navbar
-import CryptoJS from 'crypto-js';
+import MiddleSection from './MyComponents/MiddleSection'
+import './MyComponents/Navbar.css'
 
 function App() {  
+  const latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const nums = "0123456789";
+  const special = "~`!@#$%^&*()_-+=/*{}[];:'<>,."
+  const alphabet =  latin + nums +special;
   const [instring, setInstring] = useState("");
   const [outstring, setOutstring] = useState("");
+  const [value, setValue] = useDencrypt("ByteBaker",{chars : alphabet});
+  const [title1, setTitle1] = useDencrypt("Input String",{chars : alphabet});  
+  const [title2, setTitle2] = useDencrypt("Output String",{chars : alphabet});
+  const [title, setTitle]  = useDencrypt("",{chars : alphabet});
+
   const handleOnChange = (event) => {
     setInstring(event.target.value)
     setOutstring(event.target.value)
@@ -30,20 +41,33 @@ function App() {
   const handleBinary = () => {
     let res = '';
     res = instring.split('').map(char => {
-       return char.charCodeAt(0).toString(2);
+      return char.charCodeAt(0).toString(2);
     }).join(' ');
     setOutstring(res)
   }
   const handleURL = () => {
     setOutstring(encodeURI(instring))
   }
+  const handleHex = () => {
+    let hex = '';
+    for (let i = 0; i < instring.length; i++) {
+      const charCode = instring.charCodeAt(i);
+      const hexValue = charCode.toString(16);
+      hex += hexValue.padStart(2, '0');
+    }
+    setOutstring(hex)
+  }
   return (
     <>
     <div>
-      <Navbar/>
+    <nav className="navbar1">
+      <div className="navbar-title">
+        <span className="navbar-name" onClick={()=>setValue("ByteBaker")}>{value}</span>
+      </div>
+    </nav>
     </div>
     <div className="app">
-      <div className='left-section'>
+      <div className="left-section">
         <div className="button-group">
           <button className="custom-button" onClick={handleMD5}>MD5</button>
           <button className="custom-button" onClick={handleSHA1}>SHA-1</button>
@@ -53,27 +77,26 @@ function App() {
           <button className="custom-button" onClick={handleBase64}>Base64</button>
           <button className="custom-button" onClick={handleURL}>URL encoding</button>
           <button className="custom-button">HTML encoding</button>
-          <button className="custom-button">Hexadecimal</button>
+          <button className="custom-button" onClick={handleHex}>Hexadecimal</button>
           <button className="custom-button" onClick={handleBinary}>Binary</button>
           <button className="custom-button">Lorem</button>
           <button className="custom-button">Ipsum</button>
           <button className="custom-button">Lorem</button>
           <button className="custom-button">Ipsum</button>
-          <button className="custom-button">Lorem</button>
         </div>
       </div>
-      <div className='middle-section'>
-
+      <div className="middle-section">
+        <MiddleSection />
       </div>
-      <div className='right-section'>
-      <div className="section-content">
-        <span className="span-area my-2">Decrypted Message</span>
-        <textarea className="text-area" value={instring} onChange={handleOnChange}></textarea>
-        </div>
+      <div className="right-section">
         <div className="section-content">
-        <span className="span-area my-2">Encrypted Message</span>
-        <textarea className="text-area" value={outstring} onChange={handleOnChange}></textarea>
-      </div>  
+          <span className="span-area my-2" onClick={() => setTitle1("Input String")}>{title1}</span>
+          <textarea className="text-area" value={instring} onChange={handleOnChange}></textarea>
+        </div>
+          <div className="section-content">
+          <span className="span-area my-2" onClick={() => setTitle2("Output String")}>{title2}</span>
+          <textarea className="text-area" value= {outstring} onChange={handleOnChange}></textarea>
+        </div>  
       </div> 
     </div>
     </>
---
+++ src/MyComponents/MiddleSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MiddleSection = (props) => {
+    // let comp = {props.value}
+    // if(comp===1){
+    //     return (
+    //     <h1>This is MD5</h1>
+    //     );
+    // }
+};
+
+export default MiddleSection;
